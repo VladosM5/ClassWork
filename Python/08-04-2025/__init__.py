c6bf8b4ec0b85e50d2015c5d64ec06a8f9a6583c
Mikolayuk Vlad <vlad.mikolayuk@yandex.ru>
2025-04-08 21:02:35 +0300

    циклы обновление

--- Python/08-04-2025/__init__.py
+++ Python/08-04-2025/__init__.py
@@ -36,7 +36,7 @@
 #     b += k
 # print(b)
 
-# a = int(input('num1: ')) 
+# a = int(input('num1: '))
 # b = int(input('num2: '))
 # summa = 0
 # if a > b: a,b = b,a
@@ -44,3 +44,61 @@
 #     summa += a
 #     a += 1
 # print(summa)
+
+# a = int(input('num1: '))
+# b = int(input('num2: '))
+# if a > b: a,b = b,a
+# if a % 2: a += 1
+# while a <= b:
+#     print(a)
+#     a += 2
+
+# a = int(input('num1: '))
+# b = int(input('num2: '))
+# summa = 0
+# if a > b: a,b = b,a
+# while a <= b:
+#     summa += a
+#     a += 1
+# print(summa/(b-a))
+
+# x = 0
+# n = int(input('ведите число: '))
+# while n > 0:
+#     a = int(input('введите число: '))
+#     if a == 3:
+#         x += 1
+#     n -= 1
+# print(x)
+
+# summa = 0
+# n = int(input('количество: '))
+# while n > 0:
+#     a = int(input())
+#     summa += a
+#     n -= 1
+# print(summa)
+
+# maximum = 0
+# n = int(input('количество: '))
+# while n > 0:
+#     a = int(input())
+#     if maximum == 0:
+#         maximum = a
+#     if maximum < a:
+#         maximum = a
+#     n -= 1
+# print(maximum)
+
+# n = int(input('количество: '))
+# s = 0
+# while s <= 10:
+#     print(f'\t{n}\t * \t{s}\t = {n*s}')
+#     s += 1
+
+# n = int(input('введите число: '))
+# factorial = 1
+# while n > 1:
+#     factorial *= n
+#     n -= 1
+# print(factorial)
