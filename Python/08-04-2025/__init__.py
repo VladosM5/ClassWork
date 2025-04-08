6cfcfb213fbeeb6c21d3bafd181556e58da64c83
Mikolayuk Vlad <vlad.mikolayuk@yandex.ru>
2025-04-08 19:50:42 +0300

    циклы обновление

--- Python/08-04-2025/__init__.py
+++ Python/08-04-2025/__init__.py
@@ -1,1 +1,47 @@
-print("fffff")
+# a = input("введите слово ")
+# b = int(input("введите кол-во повторений "))
+# print((a+" ")*b)
+
+# n = int(input("до какого числа выводить целые числа? "))
+# a = 0
+# while a <= n:
+#     a = a + 1
+#     print(a)
+
+# n = int(input("до какого числа выводить целые числа? "))
+# while n >= 0:
+#     print(n)
+#     n -= 1
+
+# a = int(input("введите число "))
+# b = int(input("введите число "))
+# if a > b: a,b = b,a
+# while a <= b:
+#     print(a, end=' ')
+#     a += 1
+
+# a = int(input("введите число "))
+# b = int(input("введите число "))
+# if a > b: a,b = b,a
+# while a <= b:
+#     print(b, end=' ')
+#     a -= 1
+
+# a = int(input('num: '))
+# b = 0
+# k = 1
+# if a < 0: k = -1
+# while b!=a:
+#     print(b, end=' ')
+#     b += k
+# print(b)
+
+# a = int(input('num1: '))
+# b = int(input('num2: '))
+# summa = 0
+# if a > b: a,b = b,a
+# while a <= b:
+#     summa += a
+#     a += 1
+# print(summa)
+
