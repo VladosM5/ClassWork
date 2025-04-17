d3bafa05f5d99189cdfea2a21f5c3fd873bc0977
Mikolayuk Vlad <vlad.mikolayuk@yandex.ru>
2025-04-17 21:09:56 +0300

    Обновлена классная работа

--- JavaScript/17.04.2025/script.js
+++ JavaScript/17.04.2025/script.js
@@ -21,13 +21,54 @@ alert('Привет' + name); */
 // console.log("1");
 // line();
 
-let elNum = document.getElementsByClassName("num")[0];
-console.log(elNum.textContent);
+// let elNum = document.getElementsByClassName("num")[0];
+// console.log(elNum.textContent);
 
-function up() {
-   elNum.textContent++;
+// function up() {
+//    elNum.textContent++;
+// }
+
+// function down() {
+//    elNum.textContent--;
+// }
+
+// let a = 3348639469681305789684747n;
+// let b = a + 3348639469681305n;
+// console.log(typeof b + " " + b);
+
+// let b = "dfggsf";
+// b = b.indexOf("w");
+// console.log(typeof b + " " + b);
+
+// let a;
+// console.log(typeof a);
+// console.log(Math)
+
+
+num = 0
+q1 = confirm("2 * 2 = 4?");
+if (q1) {
+   num++;
+   alert("верно")
+} else {
+   alert("не верно")
+}
+q1 = confirm("2 * 2 = 5?");
+if (!q1) {
+   num++;
+   alert("верно");
+} else {
+   alert("не верно");
+}
+q1 = confirm("2 * 3 = 4?");
+if (!q1) {
+  num++;
+  alert("верно");
+} else {
+  alert("не верно");
 }
 
-function down() {
-   elNum.textContent--;
-}
+if(num==0) alert("лузер");
+if (num == 1) alert("плоховато");
+if (num == 2) alert("хорошо");
+if (num == 3) alert("отлично");
